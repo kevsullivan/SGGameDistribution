273e41d208bea93308af377b86dfc875181d6968
Kevin O'Sullivan <kevsullivan@live.ie>
2016-02-28 13:13:19 -0800

    Removing TODO's

--- Components/GameController.cs
+++ Components/GameController.cs
@@ -59,7 +59,6 @@ namespace Christoc.Modules.SGGameDistribution.Components
         /// <returns>ID of Saved Game</returns>
         public static int SaveGame(Game g, int tabId)
         {
-            //TODO: This is infinite looping for some reason. Fix Recursion to function correctly
             if (g.GameId < 1)
             {
                 g.GameId = DataProvider.Instance().AddGame(g);
